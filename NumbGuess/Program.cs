822f2f29eb964d4283131c996d9ae980edb9c018
bukhuti-vardanidze <84092360+bukhuti-vardanidze@users.noreply.github.com>
2022-11-13 16:17:04 +0400

    Number Guessing

---
+++ NumbGuess/Program.cs
@@ -0,0 +1,68 @@
+﻿namespace NumbGuess
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Random random = new Random();
+            bool playAgain = true;
+            int guess;
+            int guesses;
+            int number;
+            int min = 1;
+            int max = 100;
+            string response;
+
+            while (playAgain)
+            {
+                guess = 0;
+                guesses = 0;
+                response = "";
+                number = random.Next(min, max + 1);
+
+
+
+                while (guess != number)
+                {
+                    Console.WriteLine("Guess number between " + min + " - " + max + " : ");
+                    guess = Convert.ToInt32(Console.ReadLine());
+                    Console.WriteLine("Guess : " + guess);
+                    
+                    if (guess > number)
+                    {
+                        Console.WriteLine(guess + " is too high");
+                    }
+                    else if (guess < number)
+                    {
+                        Console.WriteLine(guess + " is too low");
+                    }
+
+                    guesses++;
+
+                }
+
+                Console.WriteLine("Number: " + number);
+                Console.WriteLine("You Win!");
+                Console.WriteLine("Guesses : " +guesses);
+
+                Console.WriteLine("Would you like to play again (Y/N): ");
+                response = Console.ReadLine();
+                response = response.ToUpper();
+
+                if (response == "Y")
+                {
+                    playAgain = true;
+                }
+                else
+                {
+                    playAgain = false;
+                }
+
+
+            }
+            Console.WriteLine("Thanks for playing! ... I guess");
+
+        }
+        
+    }
+}
